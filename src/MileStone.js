a35183d478523ed1d521e19d61b02105f0346bbd
sampathchowdary <sampathgundapuneni@gmail.com>
2023-12-04 13:13:38 -0600

    milestone updated

--- src/MileStone.js
+++ src/MileStone.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import ReactPlayer from 'react-player';
 import expression_avatars from './assets/avatars_expressions.png';
-import lipMovements from './assets/lip_movements.png'
+import lipMovements from './assets/lip_movements.png';
+import audio_generation from './assets/milestone_1.png';
 
 function MileStones() {
     return (
@@ -9,22 +11,25 @@ function MileStones() {
           <section>
           <h2 style={{color:'red'}}>MileStone 1</h2>
           <span> Converstion of text to Speech</span>
-
+          <img src={audio_generation} alt="text to speech" style={{height:'700px'}}></img>
           </section>
           <section>
           <h2 style={{color:'red'}}>MileStone 2</h2>
           <span> Finding lyp Sync frequency using audio file</span>
-          <img src={lipMovements} alt="lip momvements"></img>
+          <img src={lipMovements} alt="lip momvements" style={{height:'700px'}}></img>
           </section>
           <section>
           <h2 style={{color:'red'}}>MileStone 3</h2>
           <span> avatars generation with expressions</span>
-          <img src={expression_avatars} alt="avatars with expressions"></img>
+          <img src={expression_avatars} alt="avatars with expressions" ></img>
           </section>
           <section>
           <h2 style={{color:'red'}}>MileStone 4</h2>
           <span> animation of avatars using lyp sync frequency</span>
-
+          <div style={{maxHeight: '500px', maxWidth: '650px', marginLeft:'25%'}}>
+          <span style={{fontSize:25}}>integration of lypsync input with animation </span>
+            {/* <ReactPlayer url={videoInt}  controls={true} /> */}
+          </div>
           </section>
 
           
